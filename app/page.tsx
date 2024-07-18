79a8cbb802a2a494fd2fdfc108b5f1ad93048b51
hapnaveen <naveen.hedallaarachchi@gmail.com>
2024-07-18 12:04:54 +0530

    minor change

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import CommentList from '@/components/CommentList';
 const Home: React.FC = () => {
   return (
     <div className="container mx-auto md:p-24 grid gap-4">
-      <h1 className="text-2xl font-bold mb-4">Comment Section</h1>
+      <h1 className="text-2xl font-bold mb-4 text-center">Comment Section</h1>
       <CommentList />
       <CommentInput />
     </div>
